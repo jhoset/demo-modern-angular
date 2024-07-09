0dd1d2cb9e2e27102a4cc7506098925d804519b1
Jhoset NG <adm_dng@hotmail.com>
2024-07-09 16:12:12 -0500

    Implement imc calculator app

--- src/app/pages/demo-signals/demo-signals.component.ts
+++ src/app/pages/demo-signals/demo-signals.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {CounterComponent} from "./components/counter/counter.component";
+import {ImcCalculatorComponent} from "./components/imc-calculator/imc-calculator.component";
 
 @Component({
   selector: 'app-demo-signals',
   standalone: true,
   imports: [
-    CounterComponent
+    CounterComponent,
+    ImcCalculatorComponent
   ],
   templateUrl: './demo-signals.component.html'
 })
