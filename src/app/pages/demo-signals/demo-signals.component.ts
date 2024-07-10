cee940480ef006f0708ce08520e10b6b1be633a3
Jhoset NG <adm_dng@hotmail.com>
2024-07-10 13:10:30 -0500

    Setup base project structure - Only dashboard template kept

--- src/app/pages/demo-signals/demo-signals.component.ts
+++ src/app/pages/demo-signals/demo-signals.component.ts
@@ -1,18 +1,9 @@
 import {Component} from '@angular/core';
-import {CounterComponent} from "./components/counter/counter.component";
-import {ImcCalculatorComponent} from "./components/imc-calculator/imc-calculator.component";
-import {TodoListComponent} from "./components/todo-list/todo-list.component";
-import {UntrackedExampleComponent} from "./components/untracked-example/untracked-example.component";
 
 @Component({
   selector: 'app-demo-signals',
   standalone: true,
-  imports: [
-    CounterComponent,
-    ImcCalculatorComponent,
-    TodoListComponent,
-    UntrackedExampleComponent
-  ],
+  imports: [],
   templateUrl: './demo-signals.component.html'
 })
 export class DemoSignalsComponent {
